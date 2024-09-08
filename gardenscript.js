0a5a15cf24f07ad5be2136bc1b46085ddcdfbc7e
Chibchakum <CaptainTrips1500@proton.me>
2024-09-08 16:45:39 -0400

    completed heated gardens project and cleaned up index.html syntax

--- gardenscript.js
+++ gardenscript.js
@@ -1,12 +1,45 @@
-let next = document.querySelector('.next')
-let prev = document.querySelector('.prev')
+let items = document.querySelectorAll('.slider .item');
+let prevBtn = document.querySelector('#prev');
+let nextBtn = document.querySelector('#next');
+let lastPosition = items.length - 1;
+let firstPosition = 0;
+let active = 0;
 
-next.addEventListener('click', function() {
-    let items = document.querySelectorAll('.item')
-    document.querySelector('.slide').appendChild(items[0])
-})
+nextBtn.onclick = () => {
+    if (active < lastPosition) {
+        active++;
+        setSlider();
+    }
+}
 
-prev.addEventListener('click', function() {
-    let items = document.querySelectorAll('.item')
-    document.querySelector('.slide').prepend(items[items.length - 1])
-})
+prevBtn.onclick = () => {
+    if (active > firstPosition) {
+        active--;
+        setSlider();
+    }
+}
+
+const setSlider = () => {
+    let oldActive = document.querySelector('.slider .item.active');
+    if (oldActive) oldActive.classList.remove('active');
+    if (items[active]) {
+        items[active].classList.add('active');
+    }
+
+    nextBtn.classList.remove('d-none');
+    prevBtn.classList.remove('d-none');
+    if (active == lastPosition) nextBtn.classList.add('d-none');
+    if (active == firstPosition) prevBtn.classList.add('d-none');
+}
+
+const setDiameter = () => {
+    let slider = document.querySelector('.slider');
+    let widthSlider = slider.offsetWidth;
+    let heightSlider = slider.offsetHeight;
+    let diameter = Math.sqrt(Math.pow(widthSlider, 2) + Math.pow(heightSlider, 2));
+    document.documentElement.style.setProperty('--diameter', diameter + 'px');
+}
+
+setDiameter();
+window.addEventListener('resize', setDiameter);
+
